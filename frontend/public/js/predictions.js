c371f252abb0a08b5cd61117577fa3f602cabf6c
Wei-En Lee <weienlee@mit.edu>
2017-04-21 05:39:51 -0400

    added threshold slider

--- frontend/public/js/predictions.js
+++ frontend/public/js/predictions.js
@@ -9,4 +9,22 @@ $(function() {
     updateColorScale(scale);
   });
 
+  $(document).on('input', 'input.threshold', function(event) {
+    var t = event.target.value;
+    $('.predictions-threshold-value').html(t);
+    THRESHOLD = t;
+
+    // update scale
+    BINARY_SCALE = d3.scale.threshold()
+      .domain([THRESHOLD])
+      .range(["#D9E0E8", "#2c3e50"]);
+    SCALES["BINARY_SCALE"] = BINARY_SCALE;
+
+    // update matrix if necessary
+    var scheme = $('select.color-scheme').val();
+    if (scheme == "BINARY_SCALE") {
+      updateColorScale(scheme);
+    }
+  });
+
 });
